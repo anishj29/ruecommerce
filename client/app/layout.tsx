5a3fe23aaaf12fcfe5814f08cfa78acbf7472105
Anish Jha <anishj2900@gmail.com>
2025-02-02 13:54:55 -0500

    test

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Poppins } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 
@@ -12,17 +12,6 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const poppins = Poppins({
-  variable: "--font-poppins",
-  subsets: ["latin"],
-  weight: ["400", "700"], // Specify weights if needed
 });
 
 export const metadata: Metadata = {
