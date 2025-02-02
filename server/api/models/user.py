50eb22a8da30ff5b8808a301fc5a6b73cff36b42
sheehan maitra <maitrasheehan@gmail.com>
2025-02-01 22:52:22 -0500

    prob fixed backend

--- server/api/models/user.py
+++ server/api/models/user.py
@@ -2,12 +2,11 @@ from datetime import datetime
 from bson import ObjectId
 
 class User:
-    def __init__(self, username, email, password, img_url, location, trust_score=0):
+    def __init__(self, username, email, password, img_url = "", trust_score=0):
         self.username = username
         self.email = email
         self.password = password
-        self.img_url = img_url
-        self.location = location  
+        self.img_url = img_url 
         self.trust_score = trust_score
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -18,7 +17,6 @@ class User:
             "email": self.email,
             "password": self.password,
             "img": self.img_url,
-            "location": self.location,
             "trust_score": self.trust_score,
             "created_at": self.created_at,
             "updated_at": self.updated_at
