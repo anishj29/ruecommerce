93004db1567c46d890acc6efe14a5ece09a45bd0
ajs787 <audrey0080@gmail.com>
2025-02-02 02:55:15 -0500

    hi

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -39,53 +39,59 @@ export function Dropdown({ label }: { label: string }) {
   );
 }
 
+
 export default function Marketplace() {
   return (
-    <div className="bg-background min-h-screen flex justify-center items-center p-6">
-      <Image
-              src="/logo.png" //INSERT LOGO HERE!!
-              alt="Shop Logo"
-              className="ml-2 logo"
-              width={200}
-              height={100}
-            />
-
-      <div className="bg-foreground mt-24 text-white rounded-2xl p-6 w-full shadow-lg relative ml-20">
-        <div className="flex justify-between items-center mb-4">
-          <div className="flex items-center space-x-3">
-            
-         
   
+    <div className="bg-background min-h-screen flex justify-center items-center p-6 hello2">
+    
+      <div className="bg-foreground mt-6 text-white rounded-2xl p-4 w-full shadow-lg relative ml-20 hello3">
+        <div className="flex mb-4 hello">
+
+          <div className="font-poppins font-semibold flex space-x-4">
+          <div className="flex space-x-3">
+            <Image
+                    src="/logo.png" //INSERT LOGO HERE!!
+                    alt="Shop Logo"
+                    className="ml-2 logo"
+                    width={200}
+                    height={100}
+                  />
+        
+          </div>
+  
+            <Button className="bg-transparent text-white">Home</Button>
+            <Button className="bg-transparent text-white">Trade</Button>
+            <Button className="bg-transparent text-white">Donate</Button>
+          </div>
+          <div className="font-poppins font-semibold justify-center flex space-x-4">
+            <Link href="/selling"><Button className="bg-transparent !text-teal-400 text-xs">Switch to Selling</Button></Link>
+            <Link href="/login"><Button className="bg-transparent text-white sign-in">Sign In</Button></Link>
+            </div>
+        </div>
+
+        <div className="font-poppins font-semibold text-lg flex space-x-6 mb-10 hello"> 
+          <div className="font-poppins font-semibold text-lg flex space-x-6 mb-4">
+            <Dropdown label="Category" />
+            <Dropdown label="Campus" />
+            <Dropdown label="Price" />
+            <Dropdown label="Rating" />
           </div>
           <div className="relative w-1/2">
             <Search className="absolute left-3 top-3 text-gray-400" />
             <Input placeholder="Where are we shopping today?" className="font-poppins font-semibold bg-stone-50 text-gray-400 pl-10 pr-4 py-2 rounded-lg w-full" />
           </div>
-          <div className="font-poppins font-semibold flex space-x-4">
-            <Button className="bg-transparent text-white">Home</Button>
-            <Button className="bg-transparent text-white">Trade</Button>
-            <Button className="bg-transparent text-white">Donate</Button>
-            <Link href="/selling"><Button className="bg-transparent !text-teal-400 text-xs">Switch to Selling</Button></Link>
-            <Link href="/login"><Button className="bg-transparent text-white sign-in">Sign In</Button></Link>
-            <Button className="bg-black text-white">Contact</Button>
-          </div>
-        </div>
-
-        <div className="font-poppins font-semibold text-lg flex space-x-6 mb-10"> 
-          <Dropdown label="Categories" />
-          <Dropdown label="Distance" />
-          <Dropdown label="Rating" />
         </div>
 
         <div className="grid grid-cols-3 gap-4">
           {[1, 2, 3, 4, 5, 6].map((item) => (
-            <Card key={item} className="bg-foreground text-white p-4 rounded-lg">
+            <Card key={item} className="bg-foreground text-white p-4 rounded-lg hello2">
               <CardContent>
                 <div className="h-32 bg-gray-100 "></div>
                 <div className="mt-2">
-                  <p className="font-poppins font-semibold text-xs text-gray-400">@user_name</p>
+                  <p className="font-poppins font-semibold text-xs text-gray-600">@user_name</p>
                   <h4 className="font-poppins font-semibold text-base ">Service #{item}</h4>
-                  <div className="flex items-center text-yellow-400 text-sm">
+                  <div className="flex items-center text-yellow-300 text-sm">
                     <Star size={16} /> <span className="font-poppins font-semibold text-sm ml-1">5.0</span>
                   </div>
                   <p className="font-poppins font-semibold text-white-400 text-xs">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
